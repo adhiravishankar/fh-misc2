1887669279b7a7d3c8af612ab09f2eb2b7754983
Adhi Ravishankar <adhiravishankar@gmail.com>
2023-10-28 16:48:04 -0600

    Add manufacturers to both frontend and backend

--- aircraftNameParser/main.go
+++ aircraftNameParser/main.go
@@ -4,19 +4,30 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	mongoOptions "go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
+	loadDotEnv()
 	ctx := context.TODO()
 	mongoDB := connectToMongo(ctx)
 	aircrafts := getAircrafts(ctx, mongoDB)
-	addManufacturer(aircrafts)
+	manufacturers := loadManufacturers()
+	addManufacturer(ctx, mongoDB, aircrafts, manufacturers)
+}
+
+func loadDotEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func connectToMongo(ctx context.Context) *mongo.Database {
@@ -47,7 +58,7 @@ func getAircrafts(ctx context.Context, db *mongo.Database) []Aircraft {
 }
 
 // load from manufacturers.json
-func loadManufacturers() {
+func loadManufacturers() map[string]string {
 	// Open the file
 	file, err := os.Open("manufacturers.json")
 	if err != nil {
@@ -60,10 +71,29 @@ func loadManufacturers() {
 		fmt.Println(err)
 	}
 
+	manufacturerMap := make(map[string]string)
+	for _, manufacturer := range data {
+		manufacturerMap[manufacturer.Name] = manufacturer.ID
+	}
+
+	return manufacturerMap
 }
 
-func addManufacturer(aircraft []Aircraft) {
-
+func addManufacturer(ctx context.Context, db *mongo.Database, aircraft []Aircraft, manufacturers map[string]string) {
+	for _, a := range aircraft {
+		for mTitle, mID := range manufacturers {
+			contains := strings.Contains(a.Title, mTitle)
+			if contains {
+				title := strings.ReplaceAll(a.Title, mTitle, "")
+				title = strings.TrimSpace(title)
+				set := bson.M{"$set": bson.M{"manufacturer": mID, "title": title}}
+				_, err := db.Collection("aircraft").UpdateOne(ctx, bson.M{"_id": a.ID}, set)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+	}
 }
 
 type Manufacturer struct {
